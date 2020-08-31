2535f4ad3cef654cc9a9289ce580ccc14a337cb5
Devco2011 <devco2011@gmail.com>
2020-08-31 18:30:57 -0500

    Finished Likely Suspects

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,15 +1,14 @@
 export const CriminalHTML = (criminalObj) => {
     return `
-    <section class="criminals" class="card-criminal">
-        <div class="criminal">
+    <section class="criminal-card">
         
         <ul>
-            <li><h2>${criminalObj.name}</h2></li>
+            <li><h3>${criminalObj.name}</h3></li>
             <li>Age: ${criminalObj.age}</>
             <li>Crime: ${criminalObj.conviction}</li>
             <li>Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</li>
             <li>Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</li>
-        </div>
+        
     </section>
     `
 }

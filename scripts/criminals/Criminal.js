1513c4c3a5f66d221f9b8d5f2430d8ef0bcd636d
Devco2011 <devco2011@gmail.com>
2020-09-08 11:00:41 -0500

    alibis populate

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,3 +1,19 @@
+import { AlibiDialog } from "./AlibiDialog.js";
+const eventHub = document.querySelector(".container");
+
+eventHub.addEventListener("click", event => {
+    if (event.target.id.startsWith("associates--")) {
+        const [prefix, criminalId] = event.target.id.split("--")
+
+        const alibiEvent = new CustomEvent("associatesClicked", {
+            detail: {
+                chosenCriminal: criminalId
+            }
+        })
+        eventHub.dispatchEvent(alibiEvent);
+    }
+})
+
 export const CriminalHTML = (criminalObj) => {
     return `
     <section class="criminal-card">
@@ -8,8 +24,8 @@ export const CriminalHTML = (criminalObj) => {
             <li>Crime: ${criminalObj.conviction}</li>
             <li>Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</li>
             <li>Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</li>
-            
-        
+            <button id="associates--${criminalObj.id}">Associate Alibis</button>
+            ${AlibiDialog(criminalObj.id)}
     </section>
     `
 }
